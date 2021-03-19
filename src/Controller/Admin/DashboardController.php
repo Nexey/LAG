4505285bbc42a1dc1fdf66bc90fe11145a421a61
Liam <leliam@live.fr>
2021-03-19 20:30:04 +0100

    [ADD] Formulaire de connexion [ADD] Dashboard opérationnel avec les controlleurs CRUD

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Fourniture;
+use App\Entity\Gamme;
+use App\Entity\Produit;
+use App\Entity\ProduitFourniture;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -18,9 +22,9 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $routeBuilder = $this->get(AdminUrlGenerator::class);
-        return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl());
+        //return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl());
 
-        //return parent::index();
+        return parent::index();
     }
 
     public function configureDashboard(): Dashboard
@@ -37,6 +41,18 @@ class DashboardController extends AbstractDashboardController
 
             MenuItem::section('Role assignment'),
             MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+
+            MenuItem::section('Fournitures'),
+            MenuItem::linkToCrud('Fournitures', 'fa fa-user', Fourniture::class),
+
+            MenuItem::section('Gamme'),
+            MenuItem::linkToCrud('Gamme', 'fa fa-user', Gamme::class),
+
+            MenuItem::section('Produit'),
+            MenuItem::linkToCrud('Produit', 'fa fa-user', Produit::class),
+
+            MenuItem::section('ProduitFourniture'),
+            MenuItem::linkToCrud('ProduitFourniture', 'fa fa-user', ProduitFourniture::class),
         ];
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }

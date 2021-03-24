f57b73b944ebeb1c9d1391e4b367686d9ddbc273
Alexandre CONRAD <alexandre.conrad2@etu.univ-lorraine.fr>
2021-03-24 20:29:46 +0100

    [FIX] HomePage + Ajout de la page rentabilite

---
+++ src/Controller/RentabilitePageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RentabilitePageController extends AbstractController
+{
+    /**
+     * @Route("/rentabilite", name="app_rentabilite")
+     */
+    public function rentabilite(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        return $this->render('rentabilite/rentabilite.html.twig');
+    }
+}

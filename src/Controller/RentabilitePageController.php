accfd4f9dc16e01fbd6da58b820a28da1210277e
Alexandre CONRAD <alexandre.conrad2@etu.univ-lorraine.fr>
2021-03-25 18:54:53 +0100

    [ADD] Page de la gestion de la rentabilite

--- src/Controller/RentabilitePageController.php
+++ src/Controller/RentabilitePageController.php
@@ -2,21 +2,91 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Entity\Fourniture;
+use App\Repository\FournitureRepository;
+use App\Repository\GammeRepository;
+use App\Repository\ProduitFournitureRepository;
+use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\Criteria;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RentabilitePageController extends AbstractController
 {
     /**
      * @Route("/rentabilite", name="app_rentabilite")
      */
-    public function rentabilite(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function rentabilite(GammeRepository $gammeRepository, ProduitRepository $produitRepository, FournitureRepository $fournitureRepository): Response
     {
-        return $this->render('rentabilite/rentabilite.html.twig');
+        $gammes = $gammeRepository->findAll(); // Liste des gammes
+        $produits = array(); // Liste des produits
+        $prixFournitures = array(); // Liste des prix max d'achat de la fourniture manquante pour chaque produit
+        $nomProduitManquantGamme = array(); // Nom de la fourniture manquante pour chaque gamme
+
+        // Pour chaque produit dans la base de données
+        foreach ($produitRepository->findAll() as &$produit) {
+
+            // Les produits sont triés par gamme, on vérifie que la clé "gamme" existe dans le tableau, sinon on la crée
+            if (!array_key_exists($produit->getGamme()->getNom(), $produits))
+                $produits[$produit->getGamme()->getNom()] = array();
+
+            // Les prix des fournitures sont triés par gamme, on vérifie que la clé "gamme" existe dans le tableau, sinon on la crée
+            if (!array_key_exists($produit->getGamme()->getNom(), $prixFournitures))
+                $prixFournitures[$produit->getGamme()->getNom()] = array();
+
+            // Les noms des fournitures manquantes sont triés par gamme, on vérifie que la clé "gamme" existe dans le tableau, sinon on la crée
+            if (!array_key_exists($produit->getGamme()->getNom(), $nomProduitManquantGamme))
+                $nomProduitManquantGamme[$produit->getGamme()->getNom()] = array();
+
+            // Prix total des fournitures nécéssaires à la fabrication du produit
+            $prixFournituresTotal = 0;
+
+            // Fourniture manquante pour le produit
+            $produitFournitureManquant = null;
+
+            // Pour chaque fourniture dans le produit
+            foreach ($produit->getProduitFournitures() as &$produitFourniture) {
+
+                // Si c'est la fourniture manquante, on met une référence de côté
+                if ( $produitFourniture->getFourniture()->getPrixActuel() == null ) $produitFournitureManquant = $produitFourniture;
+
+                // Sinon on ajoute le prix de la fourniture * son nombre au prix total
+                else $prixFournituresTotal += $produitFourniture->getFourniture()->getPrixActuel() * $produitFourniture->getNbFourniture();
+            }
+
+            // On ajoute le produit dans la liste
+            array_push($produits[$produit->getGamme()->getNom()], $produit);
+
+            // Marge possible sur le produit ( sans la fourniture manquante )
+            $prixTotal = $produit->getPrixVente() - $prixFournituresTotal;
+
+            // Prix max auquel acheté la fourniture manquante
+            $prixAchatFournitureManquantMax = $prixTotal / $produitFournitureManquant->getNbFourniture();
+
+            // On ajoute le prix max dans la liste
+            $prixFournitures[$produit->getGamme()->getNom()][$produit->getNom()] = $prixAchatFournitureManquantMax;
+
+            // On ajoute le nom de la fourniture manquante dans la liste
+            $nomProduitManquantGamme[$produit->getGamme()->getNom()] = $produitFournitureManquant->getFourniture()->getNom();
+        }
+
+        $prixFicelles = $fournitureRepository->findOneBy(array('id' => 1))->getPrixActuel();
+        $prixEtiquettes = $fournitureRepository->findOneBy(array('id' => 3))->getPrixActuel();
+        $prixRubans =  $fournitureRepository->findOneBy(array('id' => 2))->getPrixActuel();
+
+        return $this->render(
+            'rentabilite/rentabilite.html.twig',
+            [
+                'gammes' => $gammes,
+                'produits' => $produits,
+                'prixFournitures' => $prixFournitures,
+                'prixEtiquettes' => $prixEtiquettes,
+                'prixRubans' => $prixRubans,
+                'prixFicelles' => $prixFicelles,
+                'nomProduitManquantGamme' => $nomProduitManquantGamme
+            ]
+        );
     }
 }
